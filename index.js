0cf91b89aebe1f0f4317f4c399f454c119567f2e
Nicoleta Matei <mateinikoleta10@gmail.com>
2022-08-06 15:51:30 +0300

    first teams list version

---
+++ index.js
@@ -0,0 +1,29 @@
+function displayTeams(teams) {
+  console.warn("display", teams);
+
+  var teamsHTML = "";
+  teams.forEach(function (team) {
+    teamsHTML += `
+  <tr>
+  <td>${team.promotion}</td>
+  <td>${team.members}</td>
+  <td>${team.name}</td>
+  <td><a href="${team.url}">open</a></td>
+  </tr> `;
+  });
+  console.info(teamsHTML);
+  document.querySelector("table tbody").innerHTML = teamsHTML;
+}
+
+function loadTeams() {
+  fetch("data/teams.json")
+    .then(function (r) {
+      console.info(r);
+      return r.json();
+    })
+    .then(function (teams) {
+      displayTeams(teams);
+    });
+}
+
+loadTeams();
